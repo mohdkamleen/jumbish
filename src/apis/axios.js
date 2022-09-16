962d51b503b788ca71705a2ef73b1d8ec09aca1b
mohdkamleen <kamleenmohd@gmail.com>
2022-09-16 22:59:02 +0530

    asdfs

--- src/apis/axios.js
+++ src/apis/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 export default axios.create({
-  // baseURL: 'https://jumbish.herokuapp.com/api', //for production mode
-  baseURL: 'http://localhost:8000/api', //for production mode
+  baseURL: 'https://jumbish.herokuapp.com/api', //for production mode
+  // baseURL: 'http://localhost:8000/api', //for production mode
   headers: {
     "Content-type": "application/json"
   }
